a84fae629c6f2f9ee66e37b86660f4a47f87fcd3
Redha-C <52258756+Redha-C@users.noreply.github.com>
2026-04-15 16:40:52 +0200

    Add: is-anagram - submission-19

---
+++ Data Structures & Algorithms/is-anagram/submission-19.py
@@ -0,0 +1,21 @@
+from collections import defaultdict
+
+class Solution:
+    def isAnagram(self, str1: str, str2: str) -> bool:
+        count_str1 = defaultdict(int)
+        count_str2 = defaultdict(int)
+
+        # If length of strings are different -> no anagrams
+        if len(str1) != len(str2):
+            return False
+        
+        # Counting the number of characters in string 1
+        for char1 in str1:
+            count_str1[char1] += 1
+
+        # Counting the number of characters in string 2 
+        for char2 in str2:
+            count_str2[char2] += 1 
+
+        return count_str1 == count_str2
+
